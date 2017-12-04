e9db50094c9aa89858e19a462123d54d9ee92a89
Signo Slichtnacht <signosn@gmail.com>
2017-12-04 10:07:41 +0000

    Fixing Issue#1
    Fiixing the dir name and the PDO connection string

--- public/app/Model/Data.php
+++ public/app/Model/Data.php
@@ -2,19 +2,19 @@
 
 class Data{
 	//You can edit the follow setting for databse
-	private $db['db_name'] = "test";
-	private $db['db_host'] = "localhost";
-	private $db['db_user'] = "root";
-	private $db['db_pass'] = "";
+	private $db_name = "test";
+	private $db_host = "localhost";
+	private $db_user = "root";
+	private $db_pass = "";
 	private $data;
 	private $database;
 
 	public function __construct()
 	{
-		$this->database = new PDO("mysql:host=".$this->db['db_host'].
-			"dbname=".$this->db['db_name'], 
-			$this->db['db_user'],
-			$this->db['db_pass']
+		$this->database = new PDO("mysql:host=".$this->db_host.
+			";dbname=".$this->db_name, 
+			$this->db_user,
+			$this->db_pass
 		);
 		$this->database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$this->database->setAttribute(PDO::ATTR_DEFAULT_FETCH, PDO::FETCH_OBJ);
